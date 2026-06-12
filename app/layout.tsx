2751244ea11462e809dca664dec00f5f19af6beb
Uniday Studio <guilhermegt13@gmail.com>
2026-06-12 17:17:53 -0300

    Microsoft Clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,19 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
   return (
     <html lang="pt-BR">
       <body>
+        <Script
+          id="microsoft-clarity"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "x61lrbhgvs");
+            `
+          }}
+        />
         {metaPixelId ? (
           <Script
             id="meta-pixel"
